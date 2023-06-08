fe069a695c91a4eb9f2b0d6b56b088ad32410f6f
Anna Patton <annapatton@MacBook-Pro-3.attlocal.net>
2023-06-07 22:14:48 -0400

    updated responsive navbar

--- javascript.js
+++ javascript.js
@@ -1,2 +1,17 @@
 let message = "Hello World";
 console.log(message); //used console.log to display message in JS
+
+// JS FOR NAVBAR 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+// IDK WHY LINE 5 ISN'T BEING READ
+hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+}))
